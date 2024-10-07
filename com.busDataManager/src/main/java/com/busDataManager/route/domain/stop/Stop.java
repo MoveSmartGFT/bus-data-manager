4323ef406ba778dd08eae3bf050c7f12db063b25
degm <degm@gft.com>
2024-10-07 18:10:20 +0200

    Workflow test #4 stop

--- com.busDataManager/src/main/java/com/busDataManager/route/domain/stop/Stop.java
+++ com.busDataManager/src/main/java/com/busDataManager/route/domain/stop/Stop.java
@@ -14,11 +14,4 @@ public class Stop {
     private String nombre;
     private List<Stop> stops;
     private List<Schedule> schedule;
-
-    public Stop(String id, String nombre, List<Stop> stops, List<Schedule> schedule) {
-        this.id = id;
-        this.nombre = nombre;
-        this.stops = stops;
-        this.schedule = schedule;
-    }
 }
